ebfbbbf51dfd2643674228eb3de946b8d38206db
Rakibstack <rh6039511@gmail.com>
2025-09-07 12:52:27 +0600

    added categories and plants card section

---
+++ style.js/index.js
@@ -0,0 +1,69 @@
+// load categories js..
+
+const loadCategories = () => {
+  fetch ("https://openapi.programming-hero.com/api/categories")
+  .then(res => res.json())
+  .then(data => displaycategories(data.categories));
+ 
+}
+
+const displaycategories =(categoris) => {
+ const categoriesEL = document.getElementById("Categories-container")
+ categoriesEL.innerHTML = ""
+  categoris.forEach(categori =>{
+  const div = document.createElement('div')
+   div.innerHTML = `  
+       <button class="mb-2">${categori.category_name}</button>
+   `
+      categoriesEL.appendChild(div)
+  }) 
+}
+
+// all plants section js
+
+const Allplants = () => {
+ fetch('https://openapi.programming-hero.com/api/plants')
+ .then(res => res.json())
+ .then(data => displayAllplants(data.plants)
+ )
+}
+// "id": 1,
+// "image": "https://i.ibb.co.com/cSQdg7tf/mango-min.jpg",
+// "name": "Mango Tree",
+// "description": "A fast-growing tropical tree that produces delicious, juicy mangoes during summer. Its dense green canopy offers shade, while its sweet fruits are rich in vitamins and minerals.",
+// "category": "Fruit Tree",
+// "price": 500
+// },
+
+ const displayAllplants = (plants) => {
+ const AllPlantsEl = document.getElementById("all-plants")
+ plants.forEach(plant => {
+ const newdiv = document.createElement("div")
+ newdiv.innerHTML =  `
+   <div class=" bg-base-100 w-56  h-auto shadow-sm rounded-xl ">
+  <figure>
+    <img
+      src="${plant.image}" class="w-full h-42 object-cover rounded-t-xl" />
+  </figure>
+  <div class=" space-y-3 p-3 ">
+    <h3 class="font-bold ">
+     ${plant.name}
+    </h3>
+    <p class="font-normal text-[12px] text-[#1F2937] ">${plant.description}</p>
+    <div class="flex justify-between m">
+      <div class="badge bg-[#DCFCE7] text-[#15803D]">${plant.category}</div>
+      <div class=" badge-outline">${plant.price}</div>
+    </div>
+    <div><button class="p-1 text-white bg-[#15803D] w-full rounded-2xl">Add to Cart</button></div>
+  </div>
+       </div>
+ `
+  AllPlantsEl.appendChild(newdiv);
+    
+ }) 
+ }
+
+
+Allplants();
+loadCategories();
+ 
